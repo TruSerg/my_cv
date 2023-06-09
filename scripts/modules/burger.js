74939d33fadcc72596ad49240a2009598d97ef22
Sergey <truhan_1984@mail.ru>
2023-06-09 12:02:05 +0300

    fixed layout

--- scripts/modules/burger.js
+++ scripts/modules/burger.js
@@ -2,16 +2,19 @@ const burgerMenu = () => {
   const burger = document.querySelector(".header-burger");
   const menu = document.querySelector(".header-navigation");
   const menuLinks = menu.querySelectorAll(".navigation-item");
+  const body = document.querySelector(".body");
 
   burger.addEventListener("click", () => {
     menu.classList.toggle("_active");
     burger.classList.toggle("_active");
+    body.classList.toggle("lock");
   });
 
   menuLinks.forEach((link) => {
     link.addEventListener("click", () => {
       menu.classList.remove("_active");
       burger.classList.remove("_active");
+      body.classList.remove("lock");
     });
   });
 
@@ -24,6 +27,7 @@ const burgerMenu = () => {
     ) {
       menu.classList.remove("_active");
       burger.classList.remove("_active");
+      body.classList.remove("lock");
     }
   });
 };
